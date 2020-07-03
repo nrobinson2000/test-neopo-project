e81069e30fd2728620462f890419c53a0eb0ac5b
Nathan Robinson <nrobinson2000@me.com>
2020-07-03 13:37:24 -0400

    build with flags at 13:37 of 2020-07-03

--- src/test-neopo-project.cpp
+++ src/test-neopo-project.cpp
@@ -1,11 +1,13 @@
 #include "Particle.h"
 
+#define STR(x) #x
+
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
 void setup() {
   Serial.begin(115200);
-  Serial.print(RELEASE_DATE);
+  Serial.print(STR(RELEASE_DATE));
 }
 
 void loop() {
